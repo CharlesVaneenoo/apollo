939473f4e974fadcdbd67f2f103f986a02b0b4c0
Jeremy MATHIAS <jeremathias@wanadoo.fr>
2016-02-14 19:51:52 +0100

    add tests for Inventory

--- game/src/test/org/apollo/game/model/inv/InventoryTest.java
+++ game/src/test/org/apollo/game/model/inv/InventoryTest.java
@@ -29,10 +29,18 @@ public class InventoryTest {
 		stackableItemInventory = new Inventory(15, Inventory.StackMode.STACK_STACKABLE_ITEMS);
 	}
 
+	/*
+	Testing method add with different parameters
+	 */
 	@Test
 	public void addTest(){
-		inventory.set(1, new Item(4151, 1));
+		inventory.add(4151,1);
 		assertTrue(inventory.contains(4151));
+		inventory.add(4152);
+		assertTrue(inventory.contains(4152));
+		Item item= new Item(4153,1);
+		inventory.add(item);
+		assertTrue(inventory.contains(4153));
 	}
 
 	@Test
@@ -64,7 +72,75 @@ public class InventoryTest {
 		stackNeverInventory.remove(1234);
 		assertEquals(stackNeverInventory.getAmount(1234),0);
 
+		Item item= new Item(4153,1);
+		inventory.add(item);
+		inventory.remove(item);
+		assertEquals(inventory.getAmount(1453),0);
+
+
 		/*TODO test when the inventory is an STACK_STACKABLE_ITEMS inventory */
 	}
 
+
+	@Test
+	public void removeSlotTest(){
+		inventory.set(1, new Item(5000,3));
+		inventory.removeSlot(1,2);
+		assertEquals(inventory.getAmount(5000),1);
+	}
+
+	@Test
+	public void resetTest(){
+		inventory.set(1, new Item(5000,3));
+		inventory.reset(1);
+		assertEquals(inventory.slotOf(5000),-1);
+		assertEquals(inventory.get(1),null);
+	}
+
+	@Test
+	public void setTest(){
+		assertEquals(inventory.get(1),null);
+		Item item= new Item(4153,1);
+		inventory.set(1, item);
+		assertEquals(inventory.get(1),item);
+		inventory.set(1, null);
+		assertEquals(inventory.get(1),null);
+	}
+
+	@Test
+	public void shiftTest(){
+		Item item= new Item(4153,1);
+		Item item2= new Item(4154,1);
+		Item item3= new Item(4155,1);
+		inventory.set(5, item);
+		inventory.set(6, item2);
+		inventory.set(9, item3);
+		inventory.shift();
+		assertEquals(inventory.get(0),item);
+		assertEquals(inventory.get(1),item2);
+		assertEquals(inventory.get(2),item3);
+	}
+
+	@Test
+	public void slotOfTest(){
+		Item item= new Item(4153,1);
+		inventory.set(1, item);
+		assertEquals(inventory.slotOf(4153),1);
+		assertEquals(inventory.slotOf(5000),-1);
+	}
+
+
+	@Test
+	public void swapTest(){
+		Item item= new Item(4153,1);
+		Item item2= new Item(4154,1);
+		inventory.set(5, item);
+		inventory.set(6, item2);
+		inventory.swap(true,5,6);
+		assertEquals(inventory.slotOf(4153),6);
+		assertEquals(inventory.slotOf(4154),5);
+		inventory.swap(true,5,6);
+		assertEquals(inventory.slotOf(4153),5);
+		assertEquals(inventory.slotOf(4154),6);
+	}
 }
