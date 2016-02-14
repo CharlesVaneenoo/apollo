54578b91f4c12bb56a2ef9c8266f30f9f67001ad
Jeremy MATHIAS <jeremathias@wanadoo.fr>
2016-02-14 21:23:51 +0100

    adding more test to Inventory

--- game/src/test/org/apollo/game/model/inv/InventoryTest.java
+++ game/src/test/org/apollo/game/model/inv/InventoryTest.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 @RunWith(PowerMockRunner.class)
@@ -62,23 +61,18 @@ public class InventoryTest {
 
 	@Test
 	public void removeTest(){
-		ItemDefinition definition;
-
 		inventory.set(1, new Item(5000,3));
 		inventory.remove(5000,2);
 		assertEquals(inventory.getAmount(5000),1);
 
-		stackNeverInventory.set(1, new Item(1234));
-		stackNeverInventory.remove(1234);
-		assertEquals(stackNeverInventory.getAmount(1234),0);
-
 		Item item= new Item(4153,1);
 		inventory.add(item);
 		inventory.remove(item);
-		assertEquals(inventory.getAmount(1453),0);
+		assertEquals(inventory.getAmount(4153),0);
 
-
-		/*TODO test when the inventory is an STACK_STACKABLE_ITEMS inventory */
+		stackNeverInventory.set(1, new Item(1234));
+		stackNeverInventory.remove(1234);
+		assertEquals(stackNeverInventory.getAmount(1234),0);
 	}
 
 
